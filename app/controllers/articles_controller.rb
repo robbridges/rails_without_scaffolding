4c962d87c514eed9e9c6a539cab2e556c02040f9
Rob Bridges <Rmb101187@gmail.com>
2022-07-12 11:50:57 -0700

    Made flash notices for errors and success on article item creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  def new
+    @article = Article.new
+  end
+  def edit
+  end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+  def update
+  end  
 end
