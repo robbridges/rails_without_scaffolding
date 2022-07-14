4a12b8912a1337de33c26d9cabe4d508f884e27f
Rob Bridges <Rmb101187@gmail.com>
2022-07-14 12:47:32 -0700

    Users upon creation now have a hashed password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase()}
   has_many :articles
   validates :username, 
-    presence: true, 
-    uniqueness: { case_sensitive: false }, 
-    length: { minimum: 3, maximum:25}
+            presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: { minimum: 3, maximum:25}
     
   validates :email, 
-  presence: true, 
-  uniqueness: { case_sensitive: false }, 
-  length: { maximum: 105}, 
-  format: { with: URI::MailTo::EMAIL_REGEXP}
+            presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: { maximum: 105}, 
+            format: { with: URI::MailTo::EMAIL_REGEXP}
+  has_secure_password
 end
