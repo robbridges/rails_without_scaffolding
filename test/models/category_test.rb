fc9db756c4d7e937a71b3e192802205bb9755c7f
Rob Bridges <Rmb101187@gmail.com>
2022-07-19 12:23:36 -0700

    Basic validations in place

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,9 +2,35 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
-  test "category should be valid" do
+  def setup
     @category = Category.new(name: "Sports")
+  end
+
+  test "category should be valid" do
+    
     assert @category.valid?
   end
 
+  test "name should be present" do
+    @category.name = ""
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save()
+    @second_category = Category.new(name: "Sports")
+    assert_not @second_category.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "a"
+    assert_not @category.valid?
+  end
+
+  
 end
